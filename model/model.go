25b3211fe23119c5ffff6aadd34608e55f66c320
a.taghipour <alitaghipour80@gmail.com>
2024-06-18 21:14:48 +0330

    Adds sorted task list handler

--- model/model.go
+++ model/model.go
@@ -1,4 +1,5 @@
 package model
 
 type Task struct {
+	Period int
 }
---
+++ utils/sorted-task-list.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"RateMonoticScheduler/model"
+	"errors"
+	"slices"
+	"sync"
+)
+
+type SortedTaskHandler interface {
+	AddTask(task *model.Task)
+	PopFirstTask() (*model.Task, error)
+}
+
+type SortedTaskHandlerImpl struct {
+	tasks []*model.Task
+	lock  sync.Mutex
+}
+
+func (s *SortedTaskHandlerImpl) AddTask(task *model.Task) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.tasks = append(s.tasks, task)
+	slices.SortFunc(s.tasks, func(a, b *model.Task) int {
+		if a.Period < b.Period {
+			return 1
+		} else if a.Period == b.Period {
+			return 0
+		} else {
+			return -1
+		}
+	})
+}
+
+func (s *SortedTaskHandlerImpl) PopFirstTask() (*model.Task, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if len(s.tasks) == 0 {
+		return nil, errors.New("dklscn")
+	}
+
+	result := s.tasks[0]
+	s.tasks = s.tasks[1:]
+
+	return result, nil
+
+}
